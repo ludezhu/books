aab9eba85d8dbaaa6894bf7bb6dddbefe1e6a64c
ludezhu <74103236+ludezhu@users.noreply.github.com>
2023-04-26 16:39:50 +0800

    Update and rename test.py to gpt_sql.py

--- books_flask/gpt_sql.py
+++ books_flask/gpt_sql.py
@@ -1,4 +1,18 @@
 
+import pymysql
+ 
+# 打开数据库连接
+db = pymysql.connect(host='localhost',
+                     user='root',
+                     password='test123',
+                     database='TESTDB')
+ 
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+ 
+# 使用 execute()  方法执行 SQL 查询 
+cursor.execute("SELECT VERSION()")
+
 args = (id, type)
 cur.execute('select id,name from user_table where id = %s and name = %s', args )
 rs=c.execute("select * from log where f_UserName=:usr",{"usr":"jetz"})
