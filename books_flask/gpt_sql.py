7c37dcc0006abe6803351ef961ea9053a176c5c9
ludezhu <74103236+ludezhu@users.noreply.github.com>
2023-04-26 16:44:20 +0800

    Update gpt_sql.py

--- books_flask/gpt_sql.py
+++ books_flask/gpt_sql.py
@@ -1,30 +1,16 @@
 
 import pymysql
  
-# 打开数据库连接
-db = pymysql.connect(host='localhost',
-                     user='root',
-                     password='test123',
-                     database='TESTDB')
- 
-# 使用 cursor() 方法创建一个游标对象 cursor
-cursor = db.cursor()
- 
-# 使用 execute()  方法执行 SQL 查询 
-cursor.execute("SELECT VERSION()")
+def query_db(id, name):
+    # 打开数据库连接
+    db = pymysql.connect(host='localhost',
+                         user='root',
+                         password='test123',
+                         database='TESTDB')
 
-args = (id, type)
-cur.execute('select id,name from user_table where id = %s and name = %s', args )
-rs=c.execute("select * from log where f_UserName=:usr",{"usr":"jetz"})
-rs=c.execute("select * from log where f_UserName=:1 ",["jetz"])
+    # 使用 cursor() 方法创建一个游标对象 cursor
+    cursor = db.cursor()
+    # 使用 execute()  方法执行 SQL 查询
+    sql = "select id,name from user_table where id = %s and name = %s" % (id, name)
+    cursor.execute(sql)
 
-cxn=psycopg2.connect(database='lap',user='postgres',password='root')
-cur = cxn.cursor()
-cur.execute('insert into t_user (username,password) VALUES (%s,%s)',('%ab','*'))
-cur.execute('select * from t_user where username = %s',("%ab",))
-rows = cur.fetchall()
-cur.execute('delete from t_user where userid = %s',(14,))
-cur.execute('select * from t_user where password = %s',("*",))
-
-sql = "select id,name from user_table where id = %s and name = %s" % (id, name)
-cur.execute(sql)
